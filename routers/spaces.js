546b0aaa6251c0bf5e9a32d9ee24674dba797b96
Serva <servas230@gmail.com>
2022-08-23 20:04:47 +0200

    Added detail page router feature 2

--- routers/spaces.js
+++ routers/spaces.js
@@ -1,22 +1,38 @@
 const { Router } = require("express");
 const Space = require("../models").space;
+const Story = require("../models").story;
 const router = new Router();
-
 
 // http :4000/spaces
 //http --ignore-stdin :4000/spaces
 //http://localhost:4000/spaces
-router.get("/", async(req, res) => {
-    try{
-        const spaceResponse = await Space.findAll();
-        console.log("spaceResponse", spaceResponse)
-        res.send(spaceResponse);
-    }catch(e){
-        console.log("spaces error:",e.message);
-        next(e);
-    }
-})
+router.get("/", async (req, res, next) => {
+  try {
+    const spaceResponse = await Space.findAll();
+    console.log("spaceResponse", spaceResponse);
+    res.send(spaceResponse);
+  } catch (e) {
+    console.log("spaces error:", e.message);
+    next(e);
+  }
+});
 
+// http :4000/spaces/1
+//http --ignore-stdin :4000/spaces/1
+//http://localhost:4000/spaces/1
 
+router.get("/:id", async (req, res, next) => {
+  try {
+    const spaceId = req.params.id;
+    console.log("spaceId", spaceId);
+    const specificSpaceWithStories = await Space.findByPk(spaceId, {
+      include: Story,
+    });
+    res.send(specificSpaceWithStories);
+  } catch (e) {
+    console.log("space detail error:", e.message);
+    next(e);
+  }
+});
 
-module.exports = router
+module.exports = router;
